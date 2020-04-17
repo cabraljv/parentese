3543f64ca7769509349d65bd052de73d7a59048e
cabraljv <joaovictorcabralsteam@gmail.com>
2020-04-16 22:25:31 -0300

    update to react-native 0.62

--- mobile/android/app/src/main/java/com/parentese/MainActivity.java
+++ mobile/android/app/src/main/java/com/parentese/MainActivity.java
@@ -1,4 +1,4 @@
-package com.parenteses;
+package com.parentese;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "parenteses";
+    return "parentese";
   }
 }
--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,17 +1,17 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
 module.exports = {
-    resolver: {
-      /* resolver options */
-     sourceExts: ['jsx','js'] //add here 
-    },
-    transformer: {
-      /* transformer options */
-    },
-    serializer: {
-      /* serializer options */
-    },
-    server: {
-      /* server options */
-    }
-  
-    /* general options */
-  };
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+};
